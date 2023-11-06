5538c7fb83c1f22f8cdb79b20d4e99a0793d3bda
adanfatmi <98587980+adanfatmi@users.noreply.github.com>
2023-11-07 03:12:35 +0530

    Add files via upload

---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import { BsFillBagFill } from "react-icons/bs";
+
+const Card = ({ img, title, star, reviews, prevPrice, newPrice }) => {
+  return (
+    <>
+      <section className="card">
+        <div className="img-div">
+          <img src={img} alt={title} className="card-img" />
+        </div>
+        
+        <div className="card-details">
+          <h3 className="card-title">{title}</h3>
+          <section className="card-reviews">
+            {star} {star} {star} {star}
+            <span className="total-reviews">{reviews}</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del className="oldprice">{prevPrice}</del> <div>$ {newPrice}</div>
+            </div>
+            <div className="bag">
+              <BsFillBagFill className="bag-icon" />
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Card;
